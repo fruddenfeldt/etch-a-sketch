31c1b1c255b8e8c97edac9447a4d131206ea4f99
Fredrik Uddenfeldt <fredrikuddenfeldt@Fredriks-MacBook-Air.local>
2023-06-03 22:50:43 +0200

    slider with pseudocode for flexible grid function

--- javascript.js
+++ javascript.js
@@ -15,8 +15,6 @@ function generateDivs (num)  {
     }
   
 
-generateDivs(2800)
-
 // Refresh button:
 const refreshButton = document.querySelector('.refreshButton')
 refreshButton.addEventListener('click', () => {location.reload()})
@@ -25,6 +23,30 @@ refreshButton.addEventListener('click', () => {location.reload()})
 // TRY THIS: https://stackoverflow.com/questions/33223511/how-can-i-make-a-40x40-grid-using-divs
 
 
+
+let slider = document.getElementById("myRange");
+let sliderValue = slider.value;
+
+slider.addEventListener("input", function() {
+  sliderValue = slider.value;
+  
+});
+
+generateDivs(100)
+
+// PSEUDOCODE FOR NEXT ITERATION:
+
+// Function that takes the slider input, e.g. a 50x50 grid
+// The actual slider value is only 50
+// This input is multipled by itself (to get 50x50)
+// the resuling value is put into the generateDivs function
+// The size of the divs also has to change accordlingly: 
+// Total area is 500x500 = 250 000 px
+// MINIMUM SIZE: 5px size, i.e. 100x100 grid size, generateDivs(10000)
+// One size above: For 6x6 pixel grid (each square has area of 36), the big square fits:
+// 250 000 / 36 = ~6944 (~83x83 sized grid)
+// For a 100x100 sized grid: 100x * 100x = 250000; 
+// MAX SIZE: 50x50 size; generateDivs(100)
 
 
 
